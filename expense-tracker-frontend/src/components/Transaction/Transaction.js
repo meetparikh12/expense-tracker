c495386936c1533f8eb41b41cbb5a919b555ebbc
meetparikh12 <parikhmeet1234@gmail.com>
2020-07-06 12:30:24 +0530

    Modified styling for Transaction and Balance Component

--- expense-tracker-frontend/src/components/Transaction/Transaction.js
+++ expense-tracker-frontend/src/components/Transaction/Transaction.js
@@ -8,10 +8,14 @@ export default function Input() {
         <div className="transaction">
             <h6 className="primary_heading text-capitalize text-white font-weight-light mb-4">Add New Transaction</h6>
             <div>
-                <input placeholder="Enter Text" className="expenseInput" value={text} type="text" onChange={(event)=> setText(event.target.value)}/>
+                <h5 className="text-white font-weight-light">Text :</h5>
+                <span className="text-white font-weight-light">(eg: Shopping, Rent, Auto Ride, Book, etc...)</span>          
+                <input placeholder="Enter Text" className="expenseInput mt-2" value={text} type="text" onChange={(event)=> setText(event.target.value)}/>
             </div>
             <div>
-                <input placeholder="Enter Amount" className="expenseInput mt-20" value={amount} type="text" onChange={(event)=> setAmount(event.target.value)}/>
+                <h5 className="text-white font-weight-light mt-4">Amount :</h5>  
+                <span className="text-white font-weight-light">(-ve: Expense, +ve: Income | eg: -50, +40)</span>          
+                <input placeholder="Enter Amount" className="expenseInput mt-2" value={amount} type="text" onChange={(event)=> setAmount(event.target.value)}/>
             </div>
             <button className="button mt-20" type="button">Add Transaction</button>
         </div>
